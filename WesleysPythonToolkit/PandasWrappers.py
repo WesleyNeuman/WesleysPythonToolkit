b84e8b853ac14ab0e7cbad4572e47d997143d5eb
Neuman, Wesley <Wesley.Neuman@gentex.com>
2021-11-11 12:05:37 -0500

    Added space trimming to stringsplit function

--- WesleysPythonToolkit/PandasWrappers.py
+++ WesleysPythonToolkit/PandasWrappers.py
@@ -6,6 +6,7 @@ def df_stringsplit(df: pd.DataFrame, colname: str, splitchar: str) -> pd.DataFra
     # Split on character and rename resulting dataframe columns
     col = df[colname].str.split(pat=splitchar, expand=True)
     for i in range(0, len(col.columns)):
+        col[col.columns[i]] = col[col.columns[i]].str.strip()
         col = col.rename(columns={i : colname + str(i)})
     # Join new dataframe back to original
     df = df.join(col, how='left')
