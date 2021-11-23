f7eebdcbd780a66b3d3c365b85c41b28791506ff
Neuman, Wesley <Wesley.Neuman@gentex.com>
2021-11-23 13:06:40 -0500

    Removed deprecation of original stringsplit as I realized I could just use regex for multiple characters. This was a good lesson...

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='WesleysPythonToolkit',
-    version='1.0.0',
+    version='1.0.1',
     packages=['WesleysPythonToolkit'],
     url='https://github.com/WesleyNeuman/WesleysPythonToolkit',
     license='Apache 2.0',
